aa96098a48604b2c035f374b8fe0b6505f3754d6
Stijnvb <stijnvb@hotmail.com>
2024-01-31 11:26:18 +0100

    Add project files.

---
+++ The Showcase/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace The_Showcase.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
