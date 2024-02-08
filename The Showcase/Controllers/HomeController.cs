390a727d24e2135a2071188450203b33680ed042
Stijnvb <stijnvb@hotmail.com>
2024-02-08 12:20:12 +0100

    .

--- The Showcase/Controllers/HomeController.cs
+++ The Showcase/Controllers/HomeController.cs
@@ -20,10 +20,6 @@ namespace The_Showcase.Controllers {
             return View();
         }
 
-        public IActionResult Profile() {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
